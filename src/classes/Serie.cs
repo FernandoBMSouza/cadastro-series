b2ea9733399f2525e742e7818b6aed45d91fcf0f
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-03-04 12:22:43 -0300

    parte 4

--- src/classes/Serie.cs
+++ src/classes/Serie.cs
@@ -9,16 +9,18 @@ namespace Cadastro_Series.src.Classes
         private string Titulo { get; set; }
         private string Descricao { get; set; }
         private int Ano { get; set; }
+        public bool Excluido { get; set; }
         #endregion
 
         #region Methods
-        public Serie(int Id, Genero genero, string titulo, string descricao, int ano)
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
         {
-            this.Id = Id;
+            this.Id = id;
             this.Genero = genero;
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -39,6 +41,11 @@ namespace Cadastro_Series.src.Classes
         public int retornaId()
         {
             return this.Id;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
         }
         #endregion
     }
